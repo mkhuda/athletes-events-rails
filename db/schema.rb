dc409f1042ef6ca3af7075067aca71472a596a64
mkhuda <mhmmd.huda@gmail.com>
2016-01-18 19:43:09 +0700

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118063718) do
+ActiveRecord::Schema.define(version: 20160118123213) do
 
   create_table "athleteevents", force: :cascade do |t|
     t.datetime "created_at",           null: false
@@ -48,5 +48,15 @@ ActiveRecord::Schema.define(version: 20160118063718) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
